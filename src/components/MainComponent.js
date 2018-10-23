15634f43cfd7be58a054cb2331b41f0673b12b2d
Rodolfo Melin <rmelin@luxoft.com>
2018-10-23 16:45:55 -0500

    React Router
    Topics seen:
    ------------
    React Router
    Right now is working the nav bar and the footer links to home and menu

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,27 +4,32 @@ import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent'
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
-
     render() {
+        const HomePage = ()=> {
+            return (
+                <Home ></Home>
+            );
+        }
+
         return (
-            <div className="container">
-               <Header></Header>
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <div>
+                <Header></Header>
+                <Switch>
+                    <Route path="/home" component={HomePage}></Route>
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" ></Redirect>
+                </Switch>
                 <Footer></Footer>
             </div>
         );
